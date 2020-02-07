6b39a962d66642f88b76f28749bb412ff403eeb0
Lasse Arkko <lasse.arkko@gmail.com>
2020-02-07 10:48:20 +0200

    Use mockRandomIndex in tests

--- src/randomElement/randomElement.test.js
+++ src/randomElement/randomElement.test.js
@@ -1,15 +1,13 @@
 const randomElement = require('./randomElement');
-
-/*Mock index selector*/
-const randomIndex = () => 0;
+const mockRandomIndex = require('../mockRandomIndex');
 
 describe('randomElement', () => {
   it('should return an element that is a member of the array', () => {
     const array = [10, 11, 12];
-    const element = randomElement(array, randomIndex);
+    const element = randomElement(array, mockRandomIndex);
     expect(array.includes(element)).toBe(true);
   });
   it('should return undefined for empty arrays', () => {
-    expect(randomElement([], randomIndex)).toBeUndefined();
+    expect(randomElement([], mockRandomIndex)).toBeUndefined();
   });
 });
--- src/randomIndices/randomIndices.test.js
+++ src/randomIndices/randomIndices.test.js
@@ -1,16 +1,14 @@
 const randomIndices = require('./randomIndices');
-
-/*Mock index selector*/
-const randomIndex = () => 0;
+const mockRandomIndex = require('../mockRandomIndex');
 
 describe('randomIndices', () => {
   it('should return empty array for empty array', () => {
-    const indices = randomIndices([], 5, randomIndex);
+    const indices = randomIndices([], 5, mockRandomIndex);
     expect(indices).toEqual([]);
   });
   it('should only return valid indices', () => {
     const array = [10, 11, 12, 13, 14, 15];
-    const indices = randomIndices(array, 6, randomIndex);
+    const indices = randomIndices(array, 6, mockRandomIndex);
     indices.forEach(index => {
       expect(index).toBeLessThan(array.length);
     });
@@ -18,12 +16,12 @@ describe('randomIndices', () => {
   it('should only return as many indices as there are elements', () => {
     const array = [10, 11, 12];
     const moreThanExist = array.length * 2;
-    const indices = randomIndices(array, moreThanExist, randomIndex);
+    const indices = randomIndices(array, moreThanExist, mockRandomIndex);
     expect(indices.length).toEqual(array.length);
   });
   it('should only return unique indices', () => {
     const array = [10, 11, 12, 13, 14, 15];
-    const indices = randomIndices(array, 6, randomIndex);
+    const indices = randomIndices(array, 6, mockRandomIndex);
     indices.forEach((element, index) => {
       indices.forEach((other, otherIndex) => {
         /*Only relevant if not the same element, which is the case
